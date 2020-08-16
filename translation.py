9b1d66e5847f2f6349f149b6292864a5e5dbebf2
viksac <65735859+viksac@users.noreply.github.com>
2020-08-16 12:33:55 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,38 +1,35 @@
 class Translation(object):
-    START_TEXT = """ Simple Telegram Renaming BOT Edited By This [MAN](https://tx.me/AID_3N)
-              🎈 @DX_BOTZ 🎈
+    START_TEXT = """ Simple Telegram Renaming BOT Edited By This [MAN](https://tx.me/naamnhibtaunga)
+              🎈 @bratflix 🎈
 <b>Please send me any Telegram file and reply /rename New Name.extn</b>
 SEE /help For My Features"""
 
     RENAME_403_ERR = "There Is Some Error With The File"
     ABS_TEXT = "What Are You Trying To Do,Mate? You Are Banned!"
-    UPGRADE_TEXT = "CONTACT @AID_3N"
+    UPGRADE_TEXT = "CONTACT @naamnhibtaunga"
     DOWNLOAD_START = "Downloading😊"
     UPLOAD_START = "Uploading✨"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @PUBLIC_LEECH_2"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Next File **"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
+    NOT_AUTH_USER_TEXT = "CONTACT @naamnhibtaunga"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @naamnhibtaunga"
     SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Set 😂😂😏"
     DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Pooyee🤣🤣"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
-    CUSTOM_CAPTION_UL_FILE = """⊱ ──────  𝗖𝗣𝗥 ────── ⊰
-𝙼𝚕𝚢𝚕𝚖   ➣@CPR_Malayalam
-𝙰𝚕𝚕        ➣@CPRLINKS
-𝚂𝚎𝚛𝚒𝚎𝚜 ➣@CPR_TV_SERIES
-𝙷𝚒𝚗𝚍𝚒   ➣@CPR_HINDI
-𝚃𝚊𝚖𝚒𝚕   ➣@CPR_Tamil
-𝙷𝙴𝚅𝙲      ➣@CPR_HEVC
-𝙸𝚗𝚏𝚘      ➣@CPR_DVDINFO"""
+    CUSTOM_CAPTION_UL_FILE = """
+<b>Join Group➣</b> <a href="https://t.me/joinchat/NHBcN0lP1By_OJGZfIbwXA">Click Here</a>
+<b>Main Channel➣</b> <a HREF="https://t.me/joinchat/AAAAAEpVEgQCfq4ZkGfabg">Click Here</a>
+<b>Backup Channel➣</b> <a href="https://t.me/joinchat/AAAAAEph9Bzx1lTl3z3hbQ">Click Here</a>
+✯ ━━ @Bratflix ━━ ✯"""
     HELP_USER = """<b>How To Use Me</b>
     1. Send me any Telegram File(Some Files Gets Error)
     2. Reply to that message to /rename NEW NAME.EXTENSION
     FEATURES
     ➡Rename Telegram File With Custom Thumbnail.
     ➡Convert Streamable File(Video) To File.
-    ➡ FOR More Join @DX_BOTZ"""
+    ➡ FOR More Join"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
     IFLONG_FILE_NAME = """Cheriya Name Aaakadeey"""
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
